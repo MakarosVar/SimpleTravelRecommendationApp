c3cb760f58b4f107e4a6123fa1dc6470a20fa223
MakisVarv <christomvarv@yahoo.com>
2026-05-13 17:34:25 +0300

    Update searchRecommendations.js

--- client/src/utils/searchRecommendations.js
+++ client/src/utils/searchRecommendations.js
@@ -29,23 +29,6 @@ export function searchRecommendations(
   return uniqueResults;
 }
 
-function shuffleArray(array) {
-  const shuffled = [...array];
-
-  for (let i = shuffled.length - 1; i > 0; i--) {
-    const randomIndex = Math.floor(Math.random() * (i + 1));
-    // const temp = shuffled[i];
-    // shuffled[i] = shuffled[randomIndex];
-    // shuffled[randomIndex] = temp;
-    [shuffled[i], shuffled[randomIndex]] = [
-      shuffled[randomIndex],
-      shuffled[i],
-    ];
-  }
-
-  return shuffled;
-}
-
 function removeDuplicates(results) {
   return results.filter(
     (place, index, self) =>
